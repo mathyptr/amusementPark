5d07a125c14d90e533f06057dc229061286b30d0
Mathyptr <mathyptr@gmail.com>
2023-11-13 18:45:18 +0100

    Project Start

---
+++ src/main/java/ingSW/amusementPark/Main.java
@@ -0,0 +1,13 @@
+package ingSW.amusementPark;
+
+/**
+ * Welcome to a Muse ment Park!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to a Muse ment Park!" );
+    }
+}
