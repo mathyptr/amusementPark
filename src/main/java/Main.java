4808d80fda3f8ff6e99eea85a043eb802b0ed2de
Mathyptr <mathyptr@gmail.com>
2023-11-19 17:12:37 +0100

    Insert log4j and Update pom file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import db.*;
 import util.MessagesBundle;
 
@@ -9,9 +14,11 @@ public class Main
 {
     public static void main( String[] args )
     {
+    	final Logger logger = LogManager.getLogger("MyLog");
+    	
      	MessagesBundle msgB= new MessagesBundle();
 		msgB.SetLanguage("en", "US");
 		System.out.println(MessagesBundle.GetResourceValue("welcome_messages"));
-//        System.out.println( "Welcome to a Muse ment Park!" );
+		logger.info(MessagesBundle.GetResourceValue("welcome_messages") + new Date());
     }
 }
