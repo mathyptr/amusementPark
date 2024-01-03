20ea7d1cd8bfa8a434127651c4de02d8a741ee1e
Samu902 <samuelemarrani@gmail.com>
2024-01-03 12:47:21 +0100

    add Customer membership interface

---
+++ src/main/java/domainModel/membership/Membership.java
@@ -0,0 +1,24 @@
+package domainModel.membership;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+
+public interface Membership {
+	 
+    String getUsesDescription();
+
+    HashMap<String, Integer> getUses();
+        
+    boolean isExpired();
+
+    LocalDate getValidFrom();
+
+    LocalDate getValidUntil();
+
+    boolean isValidForInterval(LocalDateTime start, LocalDateTime end);
+
+    float getPrice();
+
+}
