33a07f59a45138034efe8bd2b0a0aad981dc429e
Samu902 <samuelemarrani@gmail.com>
2024-01-03 12:47:05 +0100

    add Customer class

---
+++ src/main/java/domainModel/Customer.java
@@ -0,0 +1,30 @@
+package domainModel;
+import domainModel.membership.Membership;
+
+
+public class Customer extends Person {
+    private Membership membership;
+
+    public Customer(String surname, String name, String fiscalCode, Membership membership) {
+        super(surname, name, fiscalCode);
+        this.membership = membership;
+    }
+
+    public Membership getMembership() {
+        return membership;
+    }
+
+    public void setMembership(Membership membership) {
+        this.membership = membership;
+    }
+
+    @Override
+    public String toString() {
+        return "Trainer{" +
+                "fiscalCode='" + getFiscalCode() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", membership=" + getMembership() +
+                '}';
+    }
+}
