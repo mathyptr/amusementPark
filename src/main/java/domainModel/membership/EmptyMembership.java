05ad02c2d23eb07c2f689048630da72791b1e8b2
Samu902 <samuelemarrani@gmail.com>
2024-01-06 19:06:32 +0100

    add empty membership and decorator

---
+++ src/main/java/domainModel/membership/EmptyMembership.java
@@ -0,0 +1,70 @@
+package domainModel.membership;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+/**
+ * Concrete class representing a membership that can never access the the Park.
+ */
+public class EmptyMembership implements Membership {
+    /** Price of the membership */
+    private final float price;
+
+    /** Date from which the membership starts */
+    private final LocalDate validFrom;
+
+    /** Date in which the membership ends */
+    private final LocalDate validUntil;
+
+    private final HashMap<String, Integer> uses = new HashMap<>();
+
+    /**
+     * Constructor of a full membership
+     *
+     */
+    public EmptyMembership(LocalDate validFrom, LocalDate validUntil) {
+        if (!validFrom.isBefore(validUntil) && !validFrom.equals(validUntil))
+            throw new IllegalArgumentException("Date range invalid: validFrom must be a date before or equal to validUntil");
+
+        this.price = 0;
+        this.validFrom = validFrom;
+        this.validUntil = validUntil;
+    }
+
+    @Override
+    public LocalDate getValidFrom() {
+        return validFrom;
+    }
+
+    @Override
+    public LocalDate getValidUntil() {
+        return validUntil;
+    }
+    
+    @Override
+    public boolean isValidForInterval(LocalDateTime start, LocalDateTime end) {
+        return false;
+    }
+    
+    @Override
+    public boolean isExpired() {
+        return validUntil.isBefore(LocalDateTime.now().toLocalDate());
+    }
+
+    @Override
+    public String getUsesDescription() {
+        return "";
+    }
+    
+    @Override
+    public HashMap<String, Integer> getUses() {
+        return uses;
+    }
+    
+    @Override
+    public float getPrice() {
+        return price;
+    }
+ 
+}
---
+++ src/main/java/domainModel/membership/MembershipDecorator.java
@@ -0,0 +1,76 @@
+package domainModel.membership;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+/**
+ * Abstract membership decorator
+ */
+public abstract class MembershipDecorator implements Membership {
+
+    /** Membership to decorate */
+    protected final Membership membership;
+
+    /**
+     * Constructs a new membership
+     *
+     */
+    public MembershipDecorator(Membership membership) {
+        this.membership = membership;
+    }
+
+    @Override
+    public LocalDate getValidFrom() {
+        return this.membership.getValidFrom();
+    }
+
+    @Override
+    public LocalDate getValidUntil() {
+        return this.membership.getValidUntil();
+    }
+
+    @Override
+    public boolean isExpired() {
+        return this.membership.isExpired();
+    }
+
+    @Override
+    public boolean isValidForInterval(LocalDateTime start, LocalDateTime end) {
+        return this.membership.isValidForInterval(start, end);
+    }
+
+    /**
+     * Checks if the membership is valid in the given interval
+     *
+   */
+    protected boolean isDateIntervalValid(LocalDateTime start, LocalDateTime end) {
+        if (end.isBefore(start)) return false;
+        else return (this.membership.getValidFrom().isBefore(start.toLocalDate()) || this.membership.getValidFrom().equals(start.toLocalDate()))
+                && (this.membership.getValidUntil().isAfter(end.toLocalDate()) || this.membership.getValidUntil().equals(end.toLocalDate()));
+    }
+    
+    @Override
+    public String getUsesDescription() {
+        return this.membership.getUsesDescription();
+    }
+
+    @Override
+    public HashMap<String, Integer> getUses() {
+        return this.membership.getUses();
+    }
+    
+    @Override
+    public float getPrice() {
+        return this.membership.getPrice();
+    }
+
+    public int getLocalUses() {
+        return 0;
+    }
+
+    public final Membership getMembership() {
+        return this.membership;
+    }
+
+}
