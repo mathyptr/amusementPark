67b5175f0bc416c54a08d1142083b8acde257bb6
Samu902 <samuelemarrani@gmail.com>
2024-01-26 21:12:02 +0100

    Update EmployeesController.java

--- src/main/java/businessLogic/EmployeesController.java
+++ src/main/java/businessLogic/EmployeesController.java
@@ -3,9 +3,18 @@ package businessLogic;
 import dao.EmployeeDAO;
 import domainModel.Employee;
 
+
 public class EmployeesController extends PeopleController<Employee> {
 
     public EmployeesController(EmployeeDAO employeeDAO) {
         super(employeeDAO);
     }
+    
+    /**
+     * Add a new employee
+   */
+    public String addPerson(String name, String surname, String fiscalCode, float salary) throws Exception {
+    	Employee employee = new Employee(name, surname,fiscalCode,salary);
+        return super.addPerson(employee);
+    }    
 }
