09e636ccff7f9f8dadd35833523d78fb43ddd9a6
Samu902 <samuelemarrani@gmail.com>
2024-01-26 21:11:13 +0100

    Create WorkdaysMembershipDecorator.java

---
+++ src/main/java/domainModel/membership/WorkdaysMembershipDecorator.java
@@ -0,0 +1,54 @@
+package domainModel.membership;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+/**
+ * Decorator that lets the membership be valid in workdays
+ */
+public class WorkdaysMembershipDecorator extends MembershipDecorator {
+    int uses;
+
+    public WorkdaysMembershipDecorator(Membership membership) {
+        super(membership);
+    }
+
+    public WorkdaysMembershipDecorator(Membership membership, int uses) {
+        super(membership);
+        this.uses = uses;
+    }
+
+    @Override
+    public boolean isValidForInterval(LocalDateTime start, LocalDateTime end) {
+        if (!this.isDateIntervalValid(start, end)) return false;
+        boolean isCurrTrue = (isOnWeekdays(start) && isOnWeekdays(end));
+        if (isCurrTrue) uses++;
+        return super.isValidForInterval(start, end) || isCurrTrue;
+    }
+
+    @Override
+    public float getPrice() {
+        return super.getPrice() + 250;
+    }
+
+    @Override
+    public String getUsesDescription() {
+        return super.getUsesDescription() + "Weekdays uses: " + uses + ", ";
+    }
+
+    @Override
+    public HashMap<String, Integer> getUses() {
+        this.membership.getUses().put("weekdays", uses);
+        return this.membership.getUses();
+    }
+
+    private boolean isOnWeekdays(LocalDateTime date) {
+        return date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY;
+    }
+
+    @Override
+    public int getLocalUses() {
+        return uses;
+    }
+}
