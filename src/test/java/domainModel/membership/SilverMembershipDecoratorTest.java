f4b051957d803180801f8a7ab8c554cf6ac06391
Mathyptr <mathyptr@gmail.com>
2024-01-31 17:51:53 +0100

    Add class SilverMembershipDecoratorTest

---
+++ src/test/java/domainModel/membership/SilverMembershipDecoratorTest.java
@@ -0,0 +1,34 @@
+package domainModel.membership;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+
+
+class SilverMembershipDecoratorTest {
+    @Test
+    public void when_checkingIsValidForInterval_With_goodInterval_Expect_toReturnTrue() {
+        Membership m = new SilverMembershipDecorator(new EmptyMembership(LocalDate.now(), LocalDate.now().plusYears(1)));
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
+        LocalDateTime start = c.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        Assertions.assertTrue(m.isValidForInterval(start, start.plusHours(1)));
+    }
+
+    @Test
+    public void when_checkingIsValidForInterval_With_badInterval_Expect_toReturnFalse() {
+        Membership m = new SilverMembershipDecorator(new EmptyMembership(LocalDate.now(), LocalDate.now().plusYears(1)));
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
+        LocalDateTime start = c.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        Assertions.assertFalse(m.isValidForInterval(start, start.plusHours(1)));
+
+    }
+}
