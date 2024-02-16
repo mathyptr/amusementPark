1fd3b0304865bab3bf895d4d7ac035140539266f
Mathyptr <mathyptr@gmail.com>
2024-02-16 19:25:03 +0100

    add GoldMembershipDecorator Class

---
+++ src/main/java/domainModel/membership/GoldMembershipDecorator.java
@@ -0,0 +1,61 @@
+package domainModel.membership;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import util.MessagesBundle;
+
+/**
+ * Decorator that lets the membership be always valid 
+ * 
+ *  */
+public class GoldMembershipDecorator extends MembershipDecorator {
+    int uses;
+    private final float goldPrice = 500;
+	private final Logger logger = LogManager.getLogger("GoldMembershipDecorator");
+    public GoldMembershipDecorator(Membership membership) {
+        super(membership);
+    }
+
+    public GoldMembershipDecorator(Membership membership, int uses) {
+        super(membership);
+        this.uses = uses;
+    }
+
+    @Override
+    public int getLocalUses() {
+        return uses;
+    }    
+    
+    @Override
+    public String getUsesDescription() {
+        MessagesBundle msgB = MessagesBundle.getInstance();      	
+        return super.getUsesDescription() + msgB.GetResourceValue("Gold_uses") + uses + ", ";
+    }    
+
+    @Override
+    public float getPrice() {
+        return super.getPrice() + goldPrice;
+    }
+        
+    @Override
+    public boolean isValidForInterval(LocalDateTime start, LocalDateTime end) {
+        if (!this.isDateIntervalValid(start, end)) return false;
+        boolean isCurrTrue = true; 
+        if (isCurrTrue) uses++;
+        else logger.debug(MessagesBundle.GetResourceValue("Memership_Not_Valid_For_Interval")+"("+start.toString()+","+end.toString()+")");
+        return super.isValidForInterval(start, end) || isCurrTrue;
+    }
+    
+   
+    @Override
+    public HashMap<String, Integer> getUses() {
+        this.membership.getUses().put("gold", uses);
+        return this.membership.getUses();
+    }
+
+
+}
---
+++ src/test/java/domainModel/membership/GoldMembershipDecoratorTest.java
@@ -0,0 +1,43 @@
+package domainModel.membership;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import util.MessagesBundle;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+
+
+class GoldMembershipDecoratorTest {
+    @BeforeAll
+    static void setLanguage() {
+        // Set up language
+        MessagesBundle.SetLanguage("it", "IT");
+    } 	
+    @Test
+    public void when_checkingIsValidForInterval_With_goodInterval_Expect_toReturnTrue() {
+        Membership m = new GoldMembershipDecorator(new EmptyMembership(LocalDate.now(), LocalDate.now().plusYears(1)));
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
+        LocalDateTime start = c.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        Assertions.assertTrue(m.isValidForInterval(start, start.plusHours(1)));
+    }
+
+    @Test
+    public void when_checkingIsValidForInterval_With_badInterval_Expect_toReturnFalse() {
+        Membership m = new GoldMembershipDecorator(new EmptyMembership(LocalDate.now(), LocalDate.now().plusYears(1)));
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
+        LocalDateTime start = c.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        Assertions.assertFalse(m.isValidForInterval(start, start.plusHours(1)));
+
+    }
+}
