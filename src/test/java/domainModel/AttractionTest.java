9fdd5de3ecb877356cf253efc2ea6729b98ca26b
Mathyptr <mathyptr@gmail.com>
2024-02-19 17:21:26 +0100

    add AttractionTest Class

---
+++ src/test/java/domainModel/AttractionTest.java
@@ -0,0 +1,31 @@
+package domainModel;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito.*;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import java.time.LocalDateTime;
+
+
+class AttractionTest {
+    @Test
+    public void When_CreatingNewCourse_With_InvalidDates_Expect_IllegalArgumentException() {
+    	Employee employeeMock = Mockito.mock(Employee.class);
+    	String fiscalCode;
+//    	when(employeeMock.getFiscalCode()).thenReturn("MRRSML01");
+// fiscalCode=employeeMock.getFiscalCode();
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> new Attraction(0, "ABC", 10, LocalDateTime.now(), LocalDateTime.now().minusMinutes(1), employeeMock.getFiscalCode()),
+            "Expected constructor to throw, but it didn't"
+        );
+
+        LocalDateTime d = LocalDateTime.now();
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> new Attraction(0, "A", 10, d, d, employeeMock.getFiscalCode()),
+            "Expected constructor to throw, but it didn't"
+        );
+    }
+}
