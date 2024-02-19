07909f0e0b8fe9c6b205ff0a9367963933f36d46
Mathyptr <mathyptr@gmail.com>
2024-02-19 19:42:11 +0100

    fix AttractionTest Class

--- src/test/java/domainModel/AttractionTest.java
+++ src/test/java/domainModel/AttractionTest.java
@@ -3,12 +3,16 @@ package domainModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import util.MessagesBundle;
+
 import java.time.LocalDateTime;
 
 
 class AttractionTest {
     @Test
     public void When_CreatingNewCourse_With_InvalidDates_Expect_IllegalArgumentException() {
+        MessagesBundle.SetLanguage("it", "IT");
     	Employee employeeMock = Mockito.mock(Employee.class);
 //    	String fiscalCode;
     	Mockito.when(employeeMock.getFiscalCode()).thenReturn("MRRSML01");
